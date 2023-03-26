27a63b6bb90628b6c7151c50eb9d00144ee7593a
Giordi Fungula <giordifungula@gmail.com>
2023-03-26 04:16:36 +0200

    added interface and allowed for y to be solved also

---
+++ back-end/src/services/calculate/hooks/helper.ts
@@ -0,0 +1,201 @@
+import { divide, add, multiply, parse, simplify, round } from 'mathjs'
+
+interface TransformEquationHelpers {
+  rightSideCoeff: number
+  rightSideVariable: string
+  leftSideCoeff: number
+  leftSideVariable: string
+  distributedSteps: string[]
+  simplifiedSteps: string[]
+  rhsHasEmptyCoeff?: boolean
+}
+
+export const getVariableCoefficient = (side: string) => {
+  // Helper function to get the coefficient of the variable term in a side of the equation
+  const variableMatch = side.match(/[+-]?[\d]*[a-z]/g)
+  const variable = variableMatch ? variableMatch[0] : 'x'
+  const coeffMatch = variable.match(/[+-]?[\d]*/g)
+  const coeff = coeffMatch ? coeffMatch[0] : '1'
+  return { coeff: Number(coeff), variable }
+}
+export const sideLetterSplit = (side: string) => side.split('')
+
+export const sideLetter = (side: string, sideSplit: string[]) => {
+  return sideSplit[side.length - 1]
+}
+
+export const transformEquation = (lhs: string, rhs: string, helpers: TransformEquationHelpers) => {
+  // Move all the variable terms to the left-hand side of the equation
+  // and all the constant terms to the right-hand side of the equation
+
+  const {
+    rightSideCoeff,
+    rightSideVariable,
+    leftSideCoeff,
+    leftSideVariable,
+    distributedSteps,
+    rhsHasEmptyCoeff,
+    simplifiedSteps
+  } = helpers
+
+  const lhsLetterSplit = sideLetterSplit(lhs)
+  const rhsLetterSplit = sideLetterSplit(rhs)
+
+  const lhsLetter = sideLetter(lhs, lhsLetterSplit)
+  const rhsLetter = sideLetter(rhs, rhsLetterSplit)
+
+  // @ts-ignore
+  const constantOperatorLeft = parse(lhs).op
+  // @ts-ignore
+  const constantOperatorRight = !rhsHasEmptyCoeff && parse(rhs).op
+  // @ts-ignore
+  const constantOnLeftSide = parse(lhs).args[1].value
+  // @ts-ignore
+  const constantOnRightSide = !rhsHasEmptyCoeff ? parse(rhs).args[0].value : parse(rhs).value
+  // @ts-ignore
+
+  // @ts-ignore
+  const coefOperatorSignRight = simplify(rhs).op
+
+  const addLeftSide = add(leftSideCoeff, multiply(-rightSideCoeff, coefOperatorSignRight === '-' ? -1 : 1))
+
+  const constantWithSignLeft =
+    constantOperatorLeft == '+' ? ' - ' + constantOnLeftSide : ' + ' + constantOnLeftSide
+
+  // @ts-ignore
+  const constantRightSideCheck = constantOnRightSide ? constantOnRightSide : parse(rhs).args[1].value
+  // @ts-ignore
+  // @ts-ignore
+  const rhsWithCoefOnlyName = parse(rhs).args ? parse(rhs).args[1].name : parse(rhs).value
+
+  const rhsWithCoefOnly = rhsWithCoefOnlyName === 'x'
+
+  const addRightSide = add(
+    // @ts-ignore
+    constantRightSideCheck,
+    constantWithSignLeft.includes('-') ? -constantOnLeftSide : constantOnLeftSide
+  )
+
+  // @ts-ignore
+  const signsChangeConditions = ['+', '*']
+
+  const coeffSignRight = signsChangeConditions.includes(coefOperatorSignRight)
+    ? ' - ' + rightSideVariable
+    : ' + ' + rightSideVariable
+
+  // Display the steps to solve the equation
+
+  const steps = rhsHasEmptyCoeff
+    ? [
+        `Move all variable terms to LHS: ${leftSideVariable}`,
+        `Move all constant terms to RHS: ${rhs} ${constantWithSignLeft}`,
+        `Add ${constantOnLeftSide} to both sides: ${lhs} ${constantWithSignLeft} = ${rhs} ${constantWithSignLeft} `,
+        `Simplify and divide both sides by the factor: ${leftSideVariable} = ${addRightSide}`,
+        `Solution: ${lhsLetter} = ${round(Number(divide(addRightSide, leftSideCoeff)), 3)}`
+      ]
+    : [
+        `Move all variable terms to LHS: ${leftSideVariable} ${coeffSignRight}`,
+        `Move all constant terms to RHS: ${
+          rhsWithCoefOnly ? `${constantWithSignLeft}` : `${constantRightSideCheck}  ${constantWithSignLeft}`
+        }   `,
+        `Add the liked terms on LHS: 1. ${leftSideVariable}  ${coeffSignRight}  = ${addLeftSide}${lhsLetter}`,
+        `Add the liked terms on RHS: 2: ${
+          rhsWithCoefOnly
+            ? `${constantWithSignLeft}`
+            : `${constantRightSideCheck} ${constantWithSignLeft}  = ${addRightSide}`
+        }  `,
+        `Simplify and divide both sides by the factor: ${
+          rhsWithCoefOnly
+            ? `${addLeftSide}${lhsLetter} = ${constantWithSignLeft}`
+            : `${addLeftSide}${lhsLetter} = ${addRightSide}`
+        } `,
+        `Solution: ${lhsLetter} = ${
+          rhsWithCoefOnly
+            ? round(
+                Number(
+                  divide(
+                    constantWithSignLeft.includes('-') ? -constantOnLeftSide : constantOnLeftSide,
+                    addLeftSide
+                  )
+                ),
+                3
+              )
+            : round(Number(divide(addRightSide, addLeftSide)), 3)
+        }`
+      ]
+
+  const stepsDistributed = [...simplifiedSteps, ...distributedSteps, ...steps]
+  // strip solution from steps
+  const solution = stepsDistributed[stepsDistributed.length - 1].split(': ')[1].trim()
+  return { steps: stepsDistributed, solution }
+}
+
+export const breakDownEquation = (lhs: string, rhs: string, originalEquation: string) => {
+  // Simplify the equation in pre steps
+
+  const [simpLhs, simpRhs] = originalEquation.split('=').map((side) => side.trim())
+
+  const allSteps = [
+    `Original equation: ${originalEquation}`,
+    `Simplify LHS: ${simpLhs}`,
+    `Simplify RHS: ${simpRhs}`
+  ]
+
+  return { allSteps }
+}
+
+export const LHSNeedsDistrubition = (lhs: string, checkIfLHSHasBrackets: boolean) => {
+  if (!checkIfLHSHasBrackets)
+    return {
+      updatedLHS: lhs,
+      distributedSteps: []
+    }
+
+  const seperateExpr = lhs.split(')')
+  const firstPart = seperateExpr[0]
+  const secondPart = seperateExpr[1]
+  const getNumberFromFirstPart = Number(firstPart.split('(')[0])
+  const getExpressionInBrackets = firstPart.split('(')[1]
+
+  const getExpressionInBracketsParse = parse(getExpressionInBrackets)
+
+  // @ts-ignore
+  const getExpressionOperator = parse(getExpressionInBrackets).op
+
+  const operatorIsAddition = getExpressionOperator === '+'
+  const operatorIsSubtraction = getExpressionOperator === '-'
+
+  const operatorBeforeSign = operatorIsAddition ? ' + ' : operatorIsSubtraction ? ' - ' : ' '
+
+  const { coeff: coeffInBracket, variable } = getVariableCoefficient(getExpressionInBrackets)
+  // @ts-ignore
+  const constantNumberInBracket = parse(getExpressionInBrackets).args[1].value
+
+  const calcCoef = multiply(getNumberFromFirstPart, coeffInBracket)
+
+  const calcConstant = multiply(
+    getNumberFromFirstPart,
+    operatorIsAddition
+      ? +constantNumberInBracket
+      : operatorIsSubtraction
+      ? -constantNumberInBracket
+      : constantNumberInBracket
+  )
+  // @ts-ignore
+  const constantAdded = add(Number(calcConstant), Number(simplify(secondPart).value))
+
+  const distrubtedExpression = `${calcCoef}x ${
+    operatorIsSubtraction ? '' : operatorIsAddition === true && '+'
+  }${calcConstant} ${secondPart}`
+
+  const newLHS = `${calcCoef}x ${
+    operatorIsSubtraction ? '' : operatorIsAddition === true && '+'
+  } ${constantAdded}`
+
+  const distributedSteps = [
+    `Distribute LHS: ${simplify(lhs).toString()}`,
+    `Add Numbers on LHS: ${distrubtedExpression}`,
+    `Simplify liked terms on LHS: ${newLHS}`
+  ]
+  return { updatedLHS: newLHS, distributedSteps }
+}
--- back-end/src/services/calculate/hooks/index.ts
+++ back-end/src/services/calculate/hooks/index.ts
@@ -1,198 +1,28 @@
 import type { HookContext } from '../../../declarations'
-import { divide, add, multiply, parse, simplify, round } from 'mathjs'
+import {
+  LHSNeedsDistrubition,
+  getVariableCoefficient,
+  breakDownEquation,
+  transformEquation,
+  sideLetter,
+  sideLetterSplit
+} from './helper'
 
 export const ERROR_INVALID_MESSAGE = `Please ensure the format is correct: See examples "7x - 2 = 10x" or 2(4x + 3) + 6 = 2x + 2`
-
-const getVariableCoefficient = (side: string) => {
-  // Helper function to get the coefficient of the variable term in a side of the equation
-  const variableMatch = side.match(/[+-]?[\d]*x/g)
-  const variable = variableMatch ? variableMatch[0] : 'x'
-  const coeffMatch = variable.match(/[+-]?[\d]*/g)
-  const coeff = coeffMatch ? coeffMatch[0] : '1'
-  return { coeff: Number(coeff), variable }
-}
-
-const transformEquation = (
-  lhs: string,
-  rhs: string,
-  helpers: {
-    checkIfLHSHasBrackets: boolean
-    distributedSteps: string[]
-    simplifiedSteps: string[]
-    rhsHasEmptyCoeff?: boolean
-  }
-) => {
-  // Move all the variable terms to the left-hand side of the equation
-  // and all the constant terms to the right-hand side of the equation
-
-  const { coeff: leftSideCoeff, variable: leftSideVariable } = getVariableCoefficient(lhs)
-  const { coeff: rightSideCoeff, variable: rightSideVariable } = getVariableCoefficient(rhs)
-
-  const { coeff, variable } = getVariableCoefficient(lhs)
-
-  const { checkIfLHSHasBrackets, distributedSteps, rhsHasEmptyCoeff, simplifiedSteps } = helpers
-
-  // @ts-ignore
-  const constantOperatorLeft = parse(lhs).op
-  // @ts-ignore
-  const constantOperatorRight = !rhsHasEmptyCoeff && parse(rhs).op
-  // @ts-ignore
-  const constantOnLeftSide = parse(lhs).args[1].value
-  // @ts-ignore
-  const constantOnRightSide = !rhsHasEmptyCoeff ? parse(rhs).args[0].value : parse(rhs).value
-  // @ts-ignore
-
-  // @ts-ignore
-  const coefOperatorSignRight = simplify(rhs).op
-
-  const addLeftSide = add(leftSideCoeff, multiply(-rightSideCoeff, coefOperatorSignRight === '-' ? -1 : 1))
-
-  const constantWithSignLeft =
-    constantOperatorLeft == '+' ? ' - ' + constantOnLeftSide : ' + ' + constantOnLeftSide
-
-  // @ts-ignore
-  const constantRightSideCheck = constantOnRightSide ? constantOnRightSide : parse(rhs).args[1].value
-  // @ts-ignore
-  // @ts-ignore
-  const rhsWithCoefOnlyName = parse(rhs).args ? parse(rhs).args[1].name : parse(rhs).value
-
-  const rhsWithCoefOnly = rhsWithCoefOnlyName === 'x'
-
-  const addRightSide = add(
-    // @ts-ignore
-    constantRightSideCheck,
-    constantWithSignLeft.includes('-') ? -constantOnLeftSide : constantOnLeftSide
-  )
-
-  const constantWithSignRight =
-    constantOperatorRight == '+' ? ' - ' + constantOnRightSide : ' + ' + constantOnRightSide
-
-  // @ts-ignore
-  const signsChangeConditions = ['+', '*']
-
-  const coeffSignRight = signsChangeConditions.includes(coefOperatorSignRight)
-    ? ' - ' + rightSideVariable
-    : ' + ' + rightSideVariable
-
-  // Display the steps to solve the equation
-
-  const steps = rhsHasEmptyCoeff
-    ? [
-        `Move all variable terms to LHS: ${leftSideVariable}`,
-        `Move all constant terms to RHS: ${rhs} ${constantWithSignLeft}`,
-        `Add ${constantOnLeftSide} to both sides: ${lhs} ${constantWithSignLeft} = ${rhs} ${constantWithSignLeft} `,
-        `Simplify and divide both sides by the factor: ${leftSideVariable} = ${addRightSide}`,
-        `Solution: x = ${round(Number(divide(addRightSide, leftSideCoeff)), 3)}`
-      ]
-    : [
-        `Move all variable terms to LHS: ${leftSideVariable} ${coeffSignRight}`,
-        `Move all constant terms to RHS: ${
-          rhsWithCoefOnly ? `${constantWithSignLeft}` : `${constantRightSideCheck}  ${constantWithSignLeft}`
-        }   `,
-        `Add the liked terms on LHS: 1. ${leftSideVariable}  ${coeffSignRight}  = ${addLeftSide}x`,
-        `Add the liked terms on RHS: 2: ${
-          rhsWithCoefOnly
-            ? `${constantWithSignLeft}`
-            : `${constantRightSideCheck} ${constantWithSignLeft}  = ${addRightSide}`
-        }  `,
-        `Simplify and divide both sides by the factor: ${
-          rhsWithCoefOnly ? `${addLeftSide}x = ${constantWithSignLeft}` : `${addLeftSide}x = ${addRightSide}`
-        } `,
-        `Solution: x = ${
-          rhsWithCoefOnly
-            ? round(
-                Number(
-                  divide(
-                    constantWithSignLeft.includes('-') ? -constantOnLeftSide : constantOnLeftSide,
-                    addLeftSide
-                  )
-                ),
-                3
-              )
-            : round(Number(divide(addRightSide, addLeftSide)), 3)
-        }`
-      ]
-
-  const stepsDistributed = [...simplifiedSteps, ...distributedSteps, ...steps]
-  // strip solution from steps
-  const solution = stepsDistributed[stepsDistributed.length - 1].split(': ')[1].trim()
-  return { steps: stepsDistributed, solution }
-}
-
-async function breakDownEquation(lhs: string, rhs: string, originalEquation: string) {
-  // Simplify the equation in pre steps
-
-  const [simpLhs, simpRhs] = originalEquation.split('=').map((side) => side.trim())
-
-  const allSteps = [
-    `Original equation: ${originalEquation}`,
-    `Simplify LHS: ${simpLhs}`,
-    `Simplify RHS: ${simpRhs}`
-  ]
-
-  return { allSteps }
-}
-
-const LHSNeedsDistrubition = (lhs: string, checkIfLHSHasBrackets: boolean) => {
-  if (!checkIfLHSHasBrackets)
-    return {
-      updatedLHS: lhs,
-      distributedSteps: []
-    }
-
-  const seperateExpr = lhs.split(')')
-  const firstPart = seperateExpr[0]
-  const secondPart = seperateExpr[1]
-  const getNumberFromFirstPart = Number(firstPart.split('(')[0])
-  const getExpressionInBrackets = firstPart.split('(')[1]
-
-  const getExpressionInBracketsParse = parse(getExpressionInBrackets)
-
-  // @ts-ignore
-  const getExpressionOperator = parse(getExpressionInBrackets).op
-
-  const operatorIsAddition = getExpressionOperator === '+'
-  const operatorIsSubtraction = getExpressionOperator === '-'
-
-  const operatorBeforeSign = operatorIsAddition ? ' + ' : operatorIsSubtraction ? ' - ' : ' '
-
-  const { coeff: coeffInBracket, variable } = getVariableCoefficient(getExpressionInBrackets)
-  // @ts-ignore
-  const constantNumberInBracket = parse(getExpressionInBrackets).args[1].value
-
-  const calcCoef = multiply(getNumberFromFirstPart, coeffInBracket)
-
-  const calcConstant = multiply(
-    getNumberFromFirstPart,
-    operatorIsAddition
-      ? +constantNumberInBracket
-      : operatorIsSubtraction
-      ? -constantNumberInBracket
-      : constantNumberInBracket
-  )
-  // @ts-ignore
-  const constantAdded = add(Number(calcConstant), Number(simplify(secondPart).value))
-
-  const distrubtedExpression = `${calcCoef}x ${
-    operatorIsSubtraction ? '' : operatorIsAddition === true && '+'
-  }${calcConstant} ${secondPart}`
-
-  const newLHS = `${calcCoef}x ${
-    operatorIsSubtraction ? '' : operatorIsAddition === true && '+'
-  } ${constantAdded}`
-
-  const distributedSteps = [
-    `Distribute LHS: ${simplify(lhs).toString()}`,
-    `Add Numbers on LHS: ${distrubtedExpression}`,
-    `Simplify liked terms on LHS: ${newLHS}`
-  ]
-  return { updatedLHS: newLHS, distributedSteps }
-}
 
 async function solveEquation(equation: string) {
   try {
     // Step 1: Parse the input equation string
     const [lhs, rhs] = equation.split('=').map((side) => side.trim())
+
+    const { coeff: leftSideCoeff, variable: leftSideVariable } = getVariableCoefficient(lhs)
+    const { coeff: rightSideCoeff, variable: rightSideVariable } = getVariableCoefficient(rhs)
+
+    const lhsLetterSplit = sideLetterSplit(lhs)
+    const rhsLetterSplit = sideLetterSplit(rhs)
+
+    // const lhsLetter = sideLetter(lhs, lhsLetterSplit)
+    // const rhsLetter = sideLetter(rhs, rhsLetterSplit)
 
     // check if distribution is required
     const checkIfLHSHasBrackets = lhs.includes('(')
@@ -205,17 +35,19 @@ async function solveEquation(equation: string) {
     // // Step 2: Simplify the equation
     const { allSteps: preSteps } = await breakDownEquation(updatedLHS, rhs, equation)
 
-    const { coeff: rightSideCoeff, variable: rightSideVariable } = getVariableCoefficient(rhs)
-
     const hasEmptyRightCoeff = rightSideCoeff == 0
 
     // // // Step 3: Move all the variable terms to one side and all the constant terms to the other side
     const transformed = transformEquation(updatedLHS, rhs, {
-      checkIfLHSHasBrackets,
       distributedSteps,
       rhsHasEmptyCoeff: hasEmptyRightCoeff,
-      simplifiedSteps: preSteps
+      simplifiedSteps: preSteps,
+      rightSideCoeff,
+      rightSideVariable,
+      leftSideCoeff,
+      leftSideVariable
     })
+
     return transformed
   } catch (error: unknown) {
     console.log('error on solve equation', error)
